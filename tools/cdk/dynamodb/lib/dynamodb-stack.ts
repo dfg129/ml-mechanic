fbf863d2f046e8a7f3a209a3da7a925fa46034ca
dfg129 <dfg129@gmail.com>
2022-01-17 18:02:48 -0500

    update packages, mode cdk deployment

--- tools/cdk/dynamodb/lib/dynamodb-stack.ts
+++ tools/cdk/dynamodb/lib/dynamodb-stack.ts
@@ -8,43 +8,45 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import * as eventsources from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as path from 'path';
+import * as app from 'aws-cdk-lib/aws-appconfig'
 
 
 export class DynamodbStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, "rustbucket", {
-        versioned: false,
-        removalPolicy: RemovalPolicy.DESTROY,
-        enforceSSL: true,
-    });
-
     let docker_dir = path.join(__dirname, '../../../sls/dynamodb');
 
     let vpc = Vpc.fromLookup(this, "cafe-vpc", { vpcName: 'cafe-vpc' });
 
-    const dynamodb_policy = new PolicyStatement({
+    const dynamodb_create_policy = new PolicyStatement({
         actions: [
-                "dynamodb:BatchGetItem",
-				"dynamodb:Get*",
-				"dynamodb:Query",
-				"dynamodb:Scan",
-				"dynamodb:BatchWrite*",
-				"dynamodb:PutItem",
-				"dynamodb:Update*",
                 "dynamodb:ListTables",
-                "dynamodb:DescribeStream",
                 "dynamodb:DescribeTable",
                 "dynamodb:CreateTable"
         ],
         resources: ["*"]
     });
 
-    let fn = new lambda.DockerImageFunction(this, 'rust-dynamodb', {
-        description: 
-            'Rust on lambda with dynamodb',
-        code: lambda.DockerImageCode.fromImageAsset( docker_dir ), 
+    const dynamodb_read_policy = new PolicyStatement({
+        actions: [
+                "dynamodb:BatchGetItem",
+				"dynamodb:Get*",
+        ],
+        resources: ["*"]
+    });
+
+    const dynamodb_write_policy = new PolicyStatement({
+        actions: [
+				"dynamodb:BatchWrite*",
+				"dynamodb:PutItem",
+        ],
+        resources: ["*"]
+    });
+
+let fn = new lambda.DockerImageFunction(this, 'dynamodb-create-table', {
+        description:'Create a dynamodb table',
+         code: lambda.DockerImageCode.fromImageAsset( path.join(docker_dir, '/create') ),
         architecture: lambda.Architecture.ARM_64,
         environment: {
             RUST_BACKTRACE: '1',
@@ -53,12 +55,49 @@ export class DynamodbStack extends Stack {
         vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_NAT },
         logRetention: RetentionDays.ONE_WEEK,
     });
-    bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.LambdaDestination(fn));
 
     fn.role?.attachInlinePolicy(
-        new Policy(this, 'dynamodb-policy', {
-            statements: [dynamodb_policy],
+        new Policy(this, 'dynamodb-create-policy', {
+            statements: [dynamodb_create_policy],
         }),
     );
+/**
+    let fn2 = new lambda.DockerImageFunction(this, 'dynamodb-read-table', {
+        description: 'Read from a dynamodb table',
+        code: lambda.DockerImageCode.fromImageAsset(  path.join(docker_dir, '/read') ),
+        architecture: lambda.Architecture.ARM_64,
+        environment: {
+            RUST_BACKTRACE: '1',
+        } ,
+        vpc: vpc,
+        vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_NAT },
+        logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    fn2.role?.attachInlinePolicy(
+        new Policy(this, 'dynamodb-read-policy', {
+            statements: [dynamodb-read-policy],
+        }),
+    );
+
+
+    let fn3 = new lambda.DockerImageFunction(this, 'dynamodb-write-table', {
+        description: 'Write into a dynamodb table',
+        code: lambda.DockerImageCode.fromImageAsset( path.join(docker_dir, '/write' )),
+        architecture: lambda.Architecture.ARM_64,
+        environment: {
+            RUST_BACKTRACE: '1',
+        } ,
+        vpc: vpc,
+        vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_NAT },
+        logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    fn3.role?.attachInlinePolicy(
+        new Policy(this, 'dynamodb-write-policy', {
+            statements: [dynamodb-write-policy],
+        }),
+    );
+ */
   }
 }
